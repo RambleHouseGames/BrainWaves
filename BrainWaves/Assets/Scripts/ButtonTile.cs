6b1b9cb48b551e778feb90d29b4019a13e50fa4f
Quinn Larson <quinn@ramblehousegames.com>
2017-01-21 11:50:52 -0700

    ImporterWorking

---
+++ BrainWaves/Assets/Scripts/ButtonTile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTile : TileBase
+{
+	public override TileType GetTileType ()
+	{
+		return TileType.BUTTON;
+	}
+}
---
+++ BrainWaves/Assets/Scripts/DoorTile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTile : TileBase
+{
+	public override TileType GetTileType ()
+	{
+		return TileType.DOOR;
+	}
+}
