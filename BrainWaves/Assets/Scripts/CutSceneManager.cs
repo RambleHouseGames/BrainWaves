6a82ca3992e6d8b8138fc12d40bcee6d8a40ca8b
Quinn Larson <quinn@ramblehousegames.com>
2017-01-21 21:25:05 -0700

    Start Cut Scene Manager

---
+++ BrainWaves/Assets/Scripts/CutSceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CutSceneManager : MonoBehaviour 
+{
+	[SerializeField]
+	private List<Dialogue> dialogues;
+
+	void Start()
+	{
+		GameData.Instance.AddProgressCallback (onProgressChanged);
+	}
+
+	private void onProgressChanged()
+	{
+		
+	}
+}
+
+[Serializable]
+public class Dialogue
+{
+	public int progressTrigger;
+	public List<GameObject> lines;
+}
