e43a9078cc571df7ee335e1407e558d4aa000cda
Quinn Larson <quinn@ramblehousegames.com>
2017-01-21 22:17:34 -0700

    Problems with CutScene Manager

--- BrainWaves/Assets/Scripts/CutSceneManager.cs
+++ BrainWaves/Assets/Scripts/CutSceneManager.cs
@@ -8,12 +8,40 @@ public class CutSceneManager : MonoBehaviour
 	[SerializeField]
 	private List<Dialogue> dialogues;
 
+	private bool cutSceneActive = false;
+	private int lineNumber = 0;
+
 	void Start()
 	{
 		GameData.Instance.AddProgressCallback (onProgressChanged);
 	}
 
 	private void onProgressChanged()
+	{
+		Debug.Log ("onProgressChanged");
+		cutSceneActive = true;
+		GetDialogueForProgress (GameData.Instance.GetCurrentProgress ()).lines [lineNumber].transform.Rotate(new Vector3(0, 0, 90f));
+	}
+
+	private Dialogue GetDialogueForProgress(int progressNumber)
+	{
+		foreach (Dialogue dialogue in dialogues) {
+			if (dialogue.progressTrigger == progressNumber) {
+				return dialogue;
+			}
+		}
+		Debug.Assert (false, "No Dialogue for progress: " + progressNumber);
+		return null;
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			advanceDialogue ();
+		}
+	}
+
+	private void advanceDialogue()
 	{
 		
 	}
